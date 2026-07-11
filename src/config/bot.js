ce55cffb090ffca9b495c752908e7b9b88505201
EinsamerSpieler <33992195+EinsamerSpieler@users.noreply.github.com>
2026-07-11 18:07:26 +0200

    Update bot activity name for help command

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "!help für Hilfe",
+        name: "/help for help | ♥️",
         // Activity type number (0 = Playing).
         type: 4,
       },
